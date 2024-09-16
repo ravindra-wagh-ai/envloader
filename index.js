7ab180f081a7e72f496084606cc2b2d0d07a4523
Ravindra Wagh <rwagh@customtravelsolutions.com>
2024-09-16 13:06:41 +0530

    Updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import data from "./pgql.js";
 export default async () => {
   const query = "SELECT * FROM envs WHERE name= $1";
-  const result = await data.read(query, [process.env.NODE_ENV]);
+  const result = await data.read(query, [process.env.ENV_KEY]);
   const keys = result?.rows?.shift()?.value;
   const output = JSON.parse(keys);
 
