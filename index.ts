a29321264615e0f4e2fe3de00f0787a28adc72c5
Ravindra Wagh <rwagh@customtravelsolutions.com>
2024-09-23 14:34:28 +0530

    Updated

--- index.ts
+++ index.ts
@@ -4,13 +4,5 @@ export default async () => {
   const query = "SELECT * FROM envs WHERE name= $1";
   const result = await data.read(query, [process.env.ENV_KEY]);
   const output = result?.rows?.shift()?.value;
-  //process.env = { ...process.env, ...output };
-  if (fs.existsSync("app.env")) {
-    fs.unlinkSync("app.env");
-  }
-
-  let keys = Object.keys(output);
-  keys.forEach((k) => {
-    fs.appendFileSync("app.env", `${k}=${output[k]}\n`, "utf8");
-  });
+  process.env = { ...process.env, ...output };  
 };
